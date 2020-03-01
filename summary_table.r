f2a02f2c3b1d53467bfec51d7ae7e25361adc25a
liyu711 <a1923172548@gmail.com>
2020-03-01 12:33:56 -0800

    second version of the summary table and first version of the map

--- summary_table.r
+++ summary_table.r
@@ -1,1 +1,36 @@
 library(dplyr)
+
+get_summary_information = function() {
+  df_facility <- read.csv("data/Mental Health Facilities.csv", stringsAsFactors= FALSE)
+  df_countries <- read.csv("data/suicide-rates-overview-1985-to-2016.csv", stringsAsFactors = FALSE)
+  
+  df_countries_filtered <- df_countries %>%
+    filter(year %in% matches$Year) %>%
+    filter(country %in% matches$Country) %>%
+    group_by(country) %>%
+    filter(year == max(year)) %>%
+    summarise(
+      "Gpd per capita" = mean(gdp_per_capita....),
+      "Number of suicides (per 100k population)" = sum(suicides.100k.pop)
+    )
+  
+  df_facility_filtered <- df_facility %>%
+    filter(Country %in% df_countries_filtered$country) %>%
+    group_by(Country) %>%
+    summarise(
+      "total avalibility per 100k population" = sum(Mental.hospitals..per.100.000.population., 
+                                      Mental.health.units.in.general.hospitals..per.100.000.population.,
+                                      Mental.health.outpatient.facilities..per.100.000.population.,
+                                      Mental.health.day.treatment.facilities..per.100.000.population., 
+                                      Community.residential.facilities..per.100.000.population., 
+                                      na.rm = TRUE)
+    )
+  
+  df_summary <- df_countries_filtered %>%
+    mutate(
+      "Mental Health Facility Avaliable(per 100k population)" = df_facility_filtered$"total avalibility per 100k population"
+    )
+  return(df_summary)
+}
+
+summary <- get_summary_information()
