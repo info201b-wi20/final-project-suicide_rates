950a03f62216d12917952f1fac215a771d0a9093
liyu711 <a1923172548@gmail.com>
2020-03-01 13:06:10 -0800

    final? version of the mental health facility map and summary table

--- summary_table.r
+++ summary_table.r
@@ -1,8 +1,11 @@
 library(dplyr)
 
 get_summary_information = function() {
-  df_facility <- read.csv("data/Mental Health Facilities.csv", stringsAsFactors= FALSE)
-  df_countries <- read.csv("data/suicide-rates-overview-1985-to-2016.csv", stringsAsFactors = FALSE)
+  df_facility <- read.csv("data/Mental Health Facilities.csv")
+  df_countries <- read.csv("data/suicide-rates-overview-1985-to-2016.csv")
+  
+  matches <- df_facility %>%
+    select(Country, Year)
   
   df_countries_filtered <- df_countries %>%
     filter(year %in% matches$Year) %>%
@@ -19,18 +22,18 @@ get_summary_information = function() {
     group_by(Country) %>%
     summarise(
       "total avalibility per 100k population" = sum(Mental.hospitals..per.100.000.population., 
-                                      Mental.health.units.in.general.hospitals..per.100.000.population.,
-                                      Mental.health.outpatient.facilities..per.100.000.population.,
-                                      Mental.health.day.treatment.facilities..per.100.000.population., 
-                                      Community.residential.facilities..per.100.000.population., 
-                                      na.rm = TRUE)
+                                                    Mental.health.units.in.general.hospitals..per.100.000.population.,
+                                                    Mental.health.outpatient.facilities..per.100.000.population.,
+                                                    Mental.health.day.treatment.facilities..per.100.000.population., 
+                                                    Community.residential.facilities..per.100.000.population., 
+                                                    na.rm = TRUE)
     )
   
   df_summary <- df_countries_filtered %>%
     mutate(
       "Mental Health Facility Avaliable(per 100k population)" = df_facility_filtered$"total avalibility per 100k population"
     )
+  
   return(df_summary)
 }
 
-summary <- get_summary_information()
