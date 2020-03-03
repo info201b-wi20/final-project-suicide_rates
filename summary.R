c79276a48b1e00c8ae4a78a83382d9a7e579744c
Adam Ahmed <adam@Adams-MacBook-Pro.local>
2020-03-02 17:03:14 -0800

    summary script.

--- summary.R
+++ summary.R
@@ -9,26 +9,26 @@ combined_table <- read.csv("data/combined_table.csv", stringsAsFactors = FALSE)
 
 get_summary_info <- function(data_set) {
   return(list(
-    countries_no <- combined_table %>%
+    countries_no = combined_table %>%
       nrow(),
 
-    max_suicide_rate <- combined_table %>%
+    max_suicide_rate = combined_table %>%
       summarize(max_suicide = max(suicides_100kpop), na.rm = TRUE) %>%
       pull(max_suicide),
 
-    average_gdp <- combined_table %>%
+    average_gdp = combined_table %>%
       summarize(average = mean(gpd_per_capita), na.rm = TRUE) %>%
       pull(average),
 
-    average_suicide_rate <- combined_table %>%
+    average_suicide_rate = combined_table %>%
       summarize(mean_rate = mean(suicides_100kpop)) %>%
       pull(mean_rate),
 
-    average_mental_hospitals <- combined_table %>%
+    average_mental_hospitals = combined_table %>%
       summarize(mean_hospitals = mean(total_avalibility)) %>%
       pull(mean_hospitals),
     
-    col_names <- dput(names(combined_table))
+    col_names = dput(names(combined_table))
   ))
 }
 
